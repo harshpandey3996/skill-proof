22fbd5d8989abb52900c604dfc73611bf3136671
Harsh Pandey <harshpandeypbh001@gmail.com>
2026-01-16 13:26:39 +0530

    Refactor hpmodel.js for code consistency

--- Backend/model/hpmodel.js
+++ Backend/model/hpmodel.js
@@ -1,27 +1,24 @@
-const {DataTypes} = require('sequelize');
+const { DataTypes } = require("sequelize");
+const HP = require("../Config/hpconfig");
 
-const HP = require('../Config/hpconfig');
-
-const janhvi = HP.define('ayush',{
-    name : {
-        type:DataTypes.STRING,
-        allowNull:false,
-    },
-    password:{
-        type:DataTypes.STRING,
-        allowNull:false,
-    },
-    confirmpassword:{
-        type:DataTypes.STRING,
-        allowNull:false,
-    },
-    email:{
-        type:DataTypes.STRING,
-        unique:true,
-        allowNull:false,
-    },
-  
-    
+const janhvi = HP.define("ayush", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  confirmpassword: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
 });
-module.exports= janhvi;
-janhvi.sync({alter:true});
+
+module.exports = janhvi;
