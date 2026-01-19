de45470a6bb8d32c31d9b1fa4d941805045c1d5f
Harsh Pandey <harshpandeypbh001@gmail.com>
2026-01-19 15:36:08 +0530

    Update hpmodel to include email and phone fields
    Added email, phone, and otpExpiry fields to hpmodel.

--- Backend/model/hpmodel.js
+++ Backend/model/hpmodel.js
@@ -6,18 +6,39 @@ const janhvi = HP.define("ayush", {
     type: DataTypes.STRING,
     allowNull: false,
   },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  confirmpassword: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
+
   email: {
     type: DataTypes.STRING,
     unique: true,
     allowNull: false,
+  },
+
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  confirmpassword: {
+    type: DataTypes.STRING,   // ✅ STRING
+   
+    allowNull: true,
+  },
+
+
+  phone: {
+    type: DataTypes.STRING,   // ✅ STRING
+    unique: false,
+    allowNull: true,
+  },
+
+  otp: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+
+  otpExpiry: {
+    type: DataTypes.DATE,
+    allowNull: true,
   },
 });
 
