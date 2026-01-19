8d26a846f73bfd1fecdc7b876d98b44caa4e912d
Harsh Pandey <harshpandeypbh001@gmail.com>
2026-01-19 15:32:03 +0530

    Fix SSL configuration in hpconfig.js

--- Backend/Config/hpconfig.js
+++ Backend/Config/hpconfig.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
   logging: false,
-  dialectOptions: {
+  dialectOptions: { 
     ssl: {
       require: true,
       rejectUnauthorized: false,
