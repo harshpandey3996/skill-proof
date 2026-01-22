14fc82f5c9acf3b8ee8d50c94ed4eb94751e0e2a
Harsh Pandey <harshpandeypbh001@gmail.com>
2026-01-22 21:48:06 +0530

    Remove password routes from hproutes.js
    Removed password-related routes from hproutes.js.

--- Backend/routes/hproutes.js
+++ Backend/routes/hproutes.js
@@ -7,20 +7,13 @@ const {
   loginUser,
 } = require("../Controller/hpcontroller");
 
-const {
-  forgotPassword,
-  verifyOtp,
-  resetPassword,
-} = require("../Controller/passwordController");
+
 
 // user
 router.post("/post", create);
 router.post("/login", loginUser);
 router.get("/get", Findall);
 
-// password
-router.post("/forgot-password", forgotPassword);
-router.post("/verify-otp", verifyOtp);
-router.post("/reset-password", resetPassword);
+
 
 module.exports = router;
