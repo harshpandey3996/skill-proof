73807e77baf2f5a07fef4339bad1369f9102d36f
Harsh Pandey <harshpandeypbh001@gmail.com>
2026-02-09 14:30:13 +0530

    Refactor create and loginUser functions

--- Backend/Controller/hpcontroller.js
+++ Backend/Controller/hpcontroller.js
@@ -1,41 +1,21 @@
-const janhvi = require("../model/hpmodel");
-const jwt = require("jsonwebtoken");
+const janhvi = require("../model/hpmodel"); // REGISTER
 
-// REGISTER
 const create = async (req, res) => {
   try {
-    const user = await janhvi.create(req.body);
-    res.status(201).json(user);
+    const user = await janhvi.create(req.body); res.status(201).json(user);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 };
-
-// GET ALL
 const Findall = async (req, res) => {
   res.json(await janhvi.findAll());
 };
-
-
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
   const user = await janhvi.findOne({ where: { email } });
-
   if (!user) return res.status(404).json({ message: "User not found" });
-  if (user.password !== password)
-    return res.status(401).json({ message: "Invalid password" });
-
-  // JWT token generate
-  const jwtToken = jwt.sign(
-    { id: user.id },
-    process.env.JWT_SECRET,
-    { expiresIn: "7d" }
-  );
-
-  res.json({
-    token: jwtToken,
-    user: user,
-  });
+  if (user.password !== password) return res.status(401).json({ message: "Invalid password" });
+  res.json(user);
 };
 
 module.exports = { create, Findall, loginUser };
