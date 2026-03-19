7fa5c474b922d13544ac37c3793528a6f746e0f1
Harsh Pandey <harshpandeypbh001@gmail.com>
2026-03-19 21:46:14 +0530

    Refactor User model for better readability

--- Backend/model/hpmodel.js
+++ Backend/model/hpmodel.js
@@ -2,29 +2,29 @@ const { DataTypes } = require("sequelize");
 const HP = require("../Config/hpconfig");
 
 const User = HP.define("ayush", {
-  name: { type: DataTypes.STRING, allowNull: false },
+  name: { 
+    type: DataTypes.STRING, 
+    allowNull: false 
+  },
 
   email: {
     type: DataTypes.STRING,
     unique: true,
     allowNull: false,
+    validate: {
+      isEmail: true, // email validation
+    }
   },
-  confirmpassword: {
-    type: DataTypes.STRING,   // ✅ STRING
-   
-    allowNull: true,
-  },
-
 
   phone: {
-    type: DataTypes.STRING,   // ✅ STRING
-    unique: false,
+    type: DataTypes.STRING,
     allowNull: true,
   },
 
-  password: { type: DataTypes.STRING, allowNull: false },
-
- 
+  password: { 
+    type: DataTypes.STRING, 
+    allowNull: false 
+  },
 });
 
 module.exports = User;
