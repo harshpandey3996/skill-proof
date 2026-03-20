93acb1e02e3a4d2d839aec17796bff540f4e9783
Harsh Pandey <harshpandeypbh001@gmail.com>
2026-03-20 13:38:57 +0530

    Refactor User model for concise property definitions

--- Backend/model/hpmodel.js
+++ Backend/model/hpmodel.js
@@ -2,31 +2,15 @@ const { DataTypes } = require("sequelize");
 const HP = require("../Config/hpconfig");
 
 const User = HP.define("ayush", {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-
+  name: { type: DataTypes.STRING, allowNull: false },
   email: {
     type: DataTypes.STRING,
     unique: true,
     allowNull: false,
-    validate: {
-      isEmail: true, // email validation
-    }
+    validate: { isEmail: true },
   },
-
-  phone: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-
-
+  phone: { type: DataTypes.STRING, allowNull: true },
+  password: { type: DataTypes.STRING, allowNull: false },
 });
 
 module.exports = User;
