03bf6235e496beb4cb5c506a25bb78776af85349
Harsh Pandey <harshpandeypbh001@gmail.com>
2026-03-20 13:39:38 +0530

    Add route for getting completed levels

--- Backend/routes/hproutes.js
+++ Backend/routes/hproutes.js
@@ -8,18 +8,18 @@ const {
   checkProgress,
   saveProgress,
   getHistory,
+  getCompletedLevels,
 } = require("../Controller/hpcontroller");
 
-
-
-// user
+// User routes
 router.post("/post", create);
 router.post("/login", loginUser);
 router.get("/get", Findall);
+
+// Progress routes
 router.post("/check-progress", checkProgress);
 router.post("/save-progress", saveProgress);
+router.post("/get-completed-levels", getCompletedLevels); // NEW
 router.get("/history/:email", getHistory);
-
-
 
 module.exports = router;
